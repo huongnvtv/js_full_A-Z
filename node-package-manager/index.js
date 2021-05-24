8282c7cfe91a9ab90ca2cde87d4e848eef10c345
huongnv <huongitnv@gmail.com>
2021-05-24 15:46:56 +0700

    readme.md

--- node-package-manager/index.js
+++ node-package-manager/index.js
@@ -1,3 +1,5 @@
-var generatePassword = require("password-generator");
+var generatePassword = require("password-generator"); // tạo password
+var markdown = require( "markdown" ).markdown; // chuyển về dạng html
 var password =generatePassword(12, false) ;
 console.log(password);
+console.log( markdown.toHTML( "Hello *World*!" ) );
